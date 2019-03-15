8c0c8d794a3afe24ca51a79de31e342ee80b9331
mechmerlin <mechmerlin@gmail.com>
2019-03-15 13:39:18 -0700

    change keymap settings

--- keyboards/whitefox/keymaps/hanko/keymap.c
+++ keyboards/whitefox/keymaps/hanko/keymap.c
@@ -1,5 +1,5 @@
 /*
-Copyright 2015 Jun Wako <wakojun@gmail.com>
+Copyright 2019 MechMerlin, Hanko
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -16,8 +16,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
-#define KC_CTCL LCTL_T(KC_CAPS)
-
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Layer 0: Default Layer
      * ,---------------------------------------------------------------.
@@ -33,18 +31,18 @@ const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `---------------------------------------------------------------'
      */
     [0] = LAYOUT_truefox( \
-        KC_ESC,  KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME, MO(1), \
-        KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,         KC_DEL, \
-        KC_CTCL, KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                   KC_PGUP,\
-        KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                  KC_UP,   KC_PGDN,\
+        KC_GRV,  KC_1,    KC_2,    KC_3, KC_4, KC_5,   KC_6, KC_7,  KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,  MO(1), \
+        KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T,   KC_Y, KC_U,  KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,           KC_DEL, \
+        KC_ESC,  KC_A,    KC_S,    KC_D, KC_F, KC_G,   KC_H, KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,                     KC_PGUP,\
+        KC_LSFT, KC_Z,    KC_X,    KC_C, KC_V, KC_B,   KC_N, KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,                  KC_UP,     KC_PGDN,\
         KC_LCTL, KC_LALT, KC_LGUI,             KC_SPC,                       KC_RGUI, KC_RALT,                    KC_LEFT, KC_DOWN, KC_RGHT \
     ),
     
     [1] = LAYOUT_truefox( \
-        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_MUTE,\
+        KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,\
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS, KC_TRNS,          KC_TRNS,\
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_VOLU,\
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_PGUP, KC_VOLD,\
-        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,                   KC_HOME, KC_PGDN, KC_END  \
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_CAPS, KC_VOLD,\
+        KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS,                            KC_TRNS, KC_TRNS,                   KC_MPRV, KC_MPLY, KC_MNXT  \
     ),
 };
