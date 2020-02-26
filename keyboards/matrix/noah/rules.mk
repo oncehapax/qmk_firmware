563b0ba2d135cd15413f63e106bac6f091a1f4f9
mechmerlin <mechmerlin@gmail.com>
2020-02-25 18:23:21 -0800

    adjust rules

--- keyboards/matrix/noah/rules.mk
+++ keyboards/matrix/noah/rules.mk
@@ -36,7 +36,7 @@ OPT_DEFS =
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = yes      # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
 MOUSEKEY_ENABLE = no        # Mouse keys
 EXTRAKEY_ENABLE = yes	    # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
